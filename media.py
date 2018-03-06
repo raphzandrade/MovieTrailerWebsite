e62bc9000dbc2697a3469e1783d8ea8e791c55a9
Raphael Andrade <raphz.andrade@gmail.com>
2018-03-05 23:40:07 -0300

    Code review

--- media.py
+++ media.py
@@ -5,10 +5,12 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        # constructor function
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        # opens the youtube trailer url on the web browser
         webbrowser.open(self.trailer_youtube_url)
