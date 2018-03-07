a7a0ceb6ea7f8e81fc377bb0e0965deae454ea61
Raphael Andrade <raphz.andrade@gmail.com>
2018-03-06 23:31:29 -0300

    Review II

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,11 @@
 import media
 import fresh_tomatoes
 
+# creates the movie array for later use
 movies = []
 
-#creates the movie instances and append into the movies array created previously
+# creates the movie instance for 'Blade Runner'
+# and append into the movies array created previously
 movies.append(
     media.Movie(
         'Blade Runner',
@@ -11,6 +13,8 @@ movies.append(
         'https://cdn.shopify.com/s/files/1/0178/7345/products/IJ3341.jpeg?v=1491760596',
         'https://www.youtube.com/watch?v=t6GvGoNWuhw'))
 
+# creates the movie instance for 'The Shape of Water'
+# and append into the movies array created previously
 movies.append(
     media.Movie(
         'The Shape of Water',
@@ -18,6 +22,8 @@ movies.append(
         'https://2.bp.blogspot.com/-5iaMlGi_cz8/Wbrt3ll2ebI/AAAAAAAA2ZM/8Sm-cgcFuPw3iLYE5HBWrbmbK-i4OMLuQCLcBGAs/s1600/shape_of_water.jpg',
         'https://www.youtube.com/watch?v=uiA4B5Y63IQ'))
 
+# creates the movie instance for 'The lord of the Rings'
+# and append into the movies array created previously
 movies.append(
     media.Movie(
         'The Lord of the Rings: The Fellowship of the Ring',
@@ -25,6 +31,8 @@ movies.append(
         'https://www.movieartarena.com/imgs/lotr1final.jpg',
         'https://youtu.be/V75dMMIW2B4'))
 
+# creates the movie instance for 'Princess Mononoke'
+# and append into the movies array created previously
 movies.append(
     media.Movie(
         'Princess Mononoke',
@@ -32,14 +40,14 @@ movies.append(
         'https://images-na.ssl-images-amazon.com/images/I/51Xl0K7PlUL.jpg',
         'https://youtu.be/4OiMOHRDs14'))
 
+# creates the movie instance for 'Tekkonkinkreet'
+# and append into the movies array created previously
 movies.append(
     media.Movie(
         'Tekkonkinkreet',
         'placeholder',
         'https://img.fstatic.com/Yfe2gAuY1Q6F_NkXNfximYflzs8=/fit-in/290x478/smart/https://cdn.fstatic.com/media/movies/covers/2015/07/tekkonkinkreet_t9052.jpg',
         'https://youtu.be/W2ku5toXWFc'))
-
-
 
 # call the fresh_tomatoes class to open the page with the movies on the array
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,16 +1,26 @@
 import webbrowser
 
+
 class Movie():
     """ Class used to create movie instances """
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        # constructor function
+        """
+        Constructor fot the class Movie
+
+        :param title: string
+        :param storyline: string
+        :param poster_image_url: string
+        :param trailer_youtube_url: string
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        # opens the youtube trailer url on the web browser
+        """
+        This method opens the trailer on a new web browser window.
+        """
         webbrowser.open(self.trailer_youtube_url)
